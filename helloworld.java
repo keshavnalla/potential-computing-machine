6ddb5174b4883fe9b0b556f41dc06941a779804a
rakesh.keshav <rakesh.keshav@gmail.com>
2021-02-02 01:55:07 +0000

    added hello git

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 // this is sample java program to print hello world
 class {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+System.out.println("hello , git");        
+System.out.println("Hello, World!");
     }
 }
 
